504d68a338080c3f0301ceefa6113f7c10e2b6cc
Leila Eddike <eddike.leilaa@gmail.com>
2026-02-17 22:46:42 +0000

    Initial commit

---
+++ src/emailapp/Email.java
@@ -0,0 +1,11 @@
+package emailapp;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private String company;
+	private int mailBoxCapacity;
+	private String alternateEmail;
+}
---
+++ src/emailapp/EmailAddminApp.java
@@ -0,0 +1,10 @@
+package emailapp;
+
+public class EmailAddminApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
